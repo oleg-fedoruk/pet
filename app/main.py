940a5758a44ca21c56f55c022fd47c8bc973cd04
oleg <oleg.fedoruk91@gmail.com>
2023-07-12 08:46:29 +0300

    refactor: main file edited

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
-from ocr.endpoints import video_router
-from profiles.endpoints import user_router
+
+from app.ocr.api import image_router
+from app.tpdf.api import tpdf_router
 
 app = FastAPI()
-app.include_router(video_router)
-app.include_router(user_router)
+app.include_router(image_router)
+app.include_router(tpdf_router)
 
 
 if __name__ == '__main__':
